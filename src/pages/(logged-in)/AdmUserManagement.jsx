2c1137fa1814f52e98d9024b5b91ee6c697a96b6
guskka <gustavo.guska.goncalves@gmail.com>
2026-08-11 19:57:42 -0300

    💄 feat: Cria layout da searchbar para tabelas

--- src/pages/(logged-in)/AdmUserManagement.jsx
+++ src/pages/(logged-in)/AdmUserManagement.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import AsideNav from '../../components/ui/AsideNav';
 import Header from '../../components/ui/Header';
-import { PlusIcon } from '@phosphor-icons/react';
+import Input from '../../components/ui/Input';
+import {
+  CaretDownIcon,
+  MagnifyingGlassIcon,
+  PlusIcon,
+} from '@phosphor-icons/react';
 
 export default function AdmUserManagement() {
   const [activeTab, setActiveTab] = useState('users');
@@ -20,7 +25,7 @@ export default function AdmUserManagement() {
             <div className="space-y-3">
               <div className="flex items-center justify-between">
                 <div>
-                  <h3 className="font-semibold text-lg">
+                  <h3 className="font-semibold text-xl">
                     Gerenciamento de Usuários
                   </h3>
                   <p className="text-sm line text-gray-500">
@@ -60,11 +65,35 @@ export default function AdmUserManagement() {
                 </button>
               </div>
             </div>
-            <div>
-              <p>tabela</p>
-            </div>
-            <div>
-              <p>tabela</p>
+            <div className="rounded-lg border-2 border-gray-300">
+              <div className="flex items-center justify-between px-4 py-3 bg-gray-200">
+                <h4 className="font-semibold text-lg">Administradores</h4>
+                <div className="flex space-x-2">
+                  <div className="group relative flex items-center w-10 h-10 px-4 py-2 rounded-lg border border-gray-600 overflow-hidden transition-all ease-in-out focus-within:w-64">
+                    <label
+                      className="absolute left-0 flex items-center justify-center w-10 h-10 transition-all ease-in-out"
+                      htmlFor="admSearch"
+                    >
+                      <MagnifyingGlassIcon size={20} />
+                    </label>
+                    <input
+                      className="w-full h-full pl-6 opacity-0 group-focus-within:opacity-100 outline-none placeholder:text-gray-500"
+                      id="admSearch"
+                      type="text"
+                      placeholder="Pesquisar usuário..."
+                    />
+                  </div>
+                  <button className="p-2 cursor-pointer">
+                    <CaretDownIcon size={20} weight="fill" />
+                  </button>
+                </div>
+              </div>
+              <div>
+                <p>tabela</p>
+              </div>
+              <div>
+                <p>paginacao</p>
+              </div>
             </div>
           </div>
         </main>
