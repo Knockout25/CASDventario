fc413b4dda6d2a97d17ee7873a15b5de1f7134a3
guskka <gustavo.guska.goncalves@gmail.com>
2026-08-15 23:01:58 -0300

    💄 style: Modifica button do shadcn/ui e estilo header

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -2,31 +2,37 @@ import { Link } from 'react-router-dom';
 
 import { BellIcon, SignOutIcon } from '@phosphor-icons/react';
 
+import { Button } from '@/components/ui/button';
+
 export default function Header({ headerTitle, AccountName, AccountPosition }) {
   const avatarUrl = `https://api.dicebear.com/10.x/lorelei/svg?seed=${encodeURIComponent(AccountName)}`;
 
   return (
-    <header className="flex items-center justify-between w-full h-24 p-6 border-b border-gray-600">
+    <header className="flex items-center justify-between w-full h-24 px-6 border-b">
       <h2 className="font-semibold text-xl">{headerTitle}</h2>
       <div className="flex items-center space-x-4">
-        <Link className="p-2 rounded-full transition-all ease-in-out hover:bg-gray-500/15 active:bg-gray-500/25">
-          <BellIcon size={24} />
-        </Link>
-        <div className="h-6 w-px bg-gray-600" />
-        <Link className="flex items-center space-x-2">
-          <img
-            className="w-9"
-            src={avatarUrl}
-            alt={`Foto de perfil de ${AccountName}`}
-          />
-          <div>
-            <p>{AccountName}</p>
-            <p className="text-sm text-gray-500">{AccountPosition}</p>
-          </div>
-        </Link>
-        <button className="p-2 rounded-full transition-all ease-in-out cursor-pointer hover:bg-red-600/15 active:bg-red-600/25">
-          <SignOutIcon size={24} className="text-red-500" />
-        </button>
+        <Button size="icon-lg" variant="outline">
+          <Link>
+            <BellIcon />
+          </Link>
+        </Button>
+        <div className="h-6 w-px bg-accent" />
+        <Button variant="outline" size="profile">
+          <Link className="flex gap-1.5">
+            <img
+              className="w-9"
+              src={avatarUrl}
+              alt={`Foto de perfil de ${AccountName}`}
+            />
+            <div>
+              <p>{AccountName}</p>
+              <p className="text-sm text-muted-foreground">{AccountPosition}</p>
+            </div>
+          </Link>
+        </Button>
+        <Button size="icon-lg" variant="destructive">
+          <SignOutIcon />
+        </Button>
       </div>
     </header>
   );
