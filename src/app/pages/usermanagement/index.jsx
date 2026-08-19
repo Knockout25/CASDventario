6438c88dc5a9f745ab6b2fc9a1ce20dbe2d4648a
guskka <gustavo.guska.goncalves@gmail.com>
2026-08-19 14:43:43 -0300

    💄 feat(usermanagement): Altera coloração do subtítulo, botão e adiciona novas tabs

--- src/app/pages/usermanagement/index.jsx
+++ src/app/pages/usermanagement/index.jsx
@@ -4,8 +4,10 @@ import Header from '../../../components/ui/header';
 import { CaretDownIcon, CaretRightIcon, PlusIcon } from '@phosphor-icons/react';
 import SearchBar from '../../../components/ui/search-bar';
 import UsersDataTable from './columns';
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
+import { Button } from '@/components/ui/button';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 // Cria uma função para consumir os dados da API
 async function fetchUsers() {
@@ -48,42 +50,26 @@ export default function AdmUserManagement() {
                     <h3 className="font-semibold text-xl">
                       Gerenciamento de Usuários
                     </h3>
-                    <p className="text-sm line text-gray-500">
+                    <p className="text-sm line text-muted-foreground">
                       Administre e supervisione contas e privilégios de usuários
                       no sistema.
                     </p>
                   </div>
                   <div>
-                    <button className="flex items-center space-x-1 p-3 rounded-md text-white bg-yellow-500 transition-all ease-in-out hover:bg-yellow-600 active:bg-yellow-700">
+                    <Button variant="secondary" size="lg">
                       <PlusIcon />
                       <p>Adicionar Usuário</p>
-                    </button>
+                    </Button>
                   </div>
                 </div>
-                <div className="flex space-x-6">
-                  <button
-                    className={`p-2 w-32 cursor-pointer transition-all ease-in-out hover:border-b-3 hover:border-cyan-600
-                    ${
-                      activeTab === 'users'
-                        ? 'border-b-3 border-cyan-600'
-                        : 'border-b-3 border-transparent'
-                    }`}
-                    onClick={() => setActiveTab('users')}
-                  >
-                    Usuários
-                  </button>
-                  <button
-                    className={`p-2 w-32 cursor-pointer transition-all ease-in-out hover:border-b-3 hover:border-cyan-600
-                    ${
-                      activeTab === 'perms'
-                        ? 'border-b-3 border-cyan-600'
-                        : 'border-b-3 border-transparent'
-                    }`}
-                    onClick={() => setActiveTab('perms')}
-                  >
-                    Permissões
-                  </button>
-                </div>
+                <Tabs>
+                  <TabsList variant="line">
+                    <TabsTrigger value="administrators">
+                      Administradores
+                    </TabsTrigger>
+                    <TabsTrigger value="students">Alunos</TabsTrigger>
+                  </TabsList>
+                </Tabs>
               </div>
               <div className="rounded-lg border-2 border-gray-300">
                 <div className="flex items-center justify-between px-4 w-full h-16 bg-gray-200">
