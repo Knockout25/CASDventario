3f8ae288dd8ab5535ba65587f2be4133a81341ef
guskka <gustavo.guska.goncalves@gmail.com>
2026-08-19 15:10:40 -0300

    💄 feat(tabela): Atualiza visual da search bar e remove botão de retrair

--- src/app/pages/usermanagement/index.jsx
+++ src/app/pages/usermanagement/index.jsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
 import AsideNav from '../../../components/ui/aside-nav';
 import Header from '../../../components/ui/header';
-import { CaretDownIcon, CaretRightIcon, PlusIcon } from '@phosphor-icons/react';
+import { CaretDownIcon, CaretRightIcon, MagnifyingGlassIcon, PlusIcon } from '@phosphor-icons/react';
 import SearchBar from '../../../components/ui/search-bar';
 import UsersDataTable from './columns';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/app-sidebar';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Input } from '@/components/ui/input';
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+  InputGroupText,
+} from '@/components/ui/input-group';
 
 // Cria uma função para consumir os dados da API
 async function fetchUsers() {
@@ -34,7 +41,7 @@ export default function AdmUserManagement() {
 
   return (
     <SidebarProvider>
-      <div className="flex w-full h-screen font-geist">
+      <div className="flex w-full  font-geist">
         <AppSidebar />
         <div className="flex flex-1 flex-col min-w-0 w-full h-full">
           <Header
@@ -71,29 +78,20 @@ export default function AdmUserManagement() {
                   </TabsList>
                 </Tabs>
               </div>
-              <div className="rounded-lg border-2 border-gray-300">
-                <div className="flex items-center justify-between px-4 w-full h-16 bg-gray-200">
+              <div className="rounded-md border-2">
+                <div className="flex items-center justify-between rounded-t-md  px-4 w-full h-16 bg-card">
                   <h4 className="font-semibold text-lg">Administradores</h4>
                   <div className="flex space-x-2 transition-all ease-in-out">
-                    <SearchBar
-                      value={globalFilter}
-                      onChange={(e) => setGlobalFilter(e.target.value)}
-                    />
-                    {showUsers ? (
-                      <button
-                        onClick={() => setShowUsers(!showUsers)}
-                        className="p-2 cursor-pointer"
-                      >
-                        <CaretDownIcon size={20} weight="fill" />
-                      </button>
-                    ) : (
-                      <button
-                        onClick={() => setShowUsers(!showUsers)}
-                        className="p-2 cursor-pointer"
-                      >
-                        <CaretRightIcon size={20} weight="fill" />
-                      </button>
-                    )}
+                    <InputGroup>
+                      <InputGroupInput
+                        placeholder="Buscar usuário..."
+                        value={globalFilter}
+                        onChange={(e) => setGlobalFilter(e.target.value)}
+                      />
+                      <InputGroupAddon>
+                        <MagnifyingGlassIcon />
+                      </InputGroupAddon>
+                    </InputGroup>
                   </div>
                 </div>
                 <div
